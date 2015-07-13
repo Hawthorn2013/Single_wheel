dfa85f6442961cf8b97b391576502a4dd4adec78
yechuan0102 <yechuan0102@hotmail.com>
2015-07-13 15:30:22 +0800

    速度闭环修正

--- Sources/main.c
+++ Sources/main.c
@@ -9,21 +9,22 @@ void main(void)
 	uint16_t Data_H=0x0000;
 	uint16_t Data_L=0x0000;
 	init_all_and_POST();
+	set_speed_target(20);
 	for(;;)
 	{
-		delay_ms(20);
+	/*	delay_ms(20);
 		while(!Reg_Read(ACCEL_XOUT_H,&Data_H)){}
 		while(!Reg_Read(ACCEL_XOUT_L,&Data_L)){}
 		Data_H=Data_H<<8;
-		Data=Data_H|Data_L;
+		Data=Data_H|Data_L;*/
 		
 		if(g_Control)
 		{
 			g_Control=0;
 			count++;
-			angle_read(AngleResult);
-			//angle_calculate();
 			set_speed_pwm();
+			/*angle_read(AngleResult);
+			angle_calculate();
 			AngleControl();
 			if(AngleCalculate[0]<27&&AngleCalculate[0]>-48)
 			{ 
@@ -32,16 +33,19 @@ void main(void)
 			else
 			   {
 				set_motor_pwm(0);
-			   }
+			   }*/
+			motor_control();
 			if(count==4)
 			{
-				get_speed_now();
 				SpeedCountFlag++;
-				if(SpeedCountFlag>=20) 
+				if(SpeedCountFlag>=2) 
 				{
-					set_speed_PID();
+		//			LCD_PrintoutInt(5, 0,data_encoder.speed_now);
+				//	set_speed_PID();
 					contorl_speed_encoder_pid();
 					SpeedCountFlag=0;
+					
+					
 				}
 			}
 			else if(count==5)
